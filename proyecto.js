6c5476e68a06ed261bd46251c765182972183781
LourdesVidela <124083358+LourdesVidela@users.noreply.github.com>
2023-01-31 20:28:11 -0300

    Add files via upload

---
+++ proyecto.js
@@ -0,0 +1,5 @@
+
+onclick=document.getElementById('boton').onclick = function() {
+    console.log ("Capturamos el evento click");
+    document.getElementById('demo').innerHTML = "DNI: 21.000.000"
+}
